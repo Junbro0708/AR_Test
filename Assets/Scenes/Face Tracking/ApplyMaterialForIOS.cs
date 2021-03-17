ff74b7d3c86e750b417a6b9ded40c5bcc60b3de7
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-03-17 22:26:18 +0900

    AR

---
+++ Assets/Scenes/Face Tracking/ApplyMaterialForIOS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyMaterialForIOS : MonoBehaviour
+{
+    public Material[] materialForIOS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_IOS
+        GetComponent<MeshRenderer>().materials = materialForIOS;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
